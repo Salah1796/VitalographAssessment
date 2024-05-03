65c656841e6c18776727dfd323489321cdd8db46
SalahMahmoud22 <33803510+Salah1796@users.noreply.github.com>
2024-05-03 17:35:28 +0300

    Old solution

--- VitalographAssessment/CaesarCipher/Program.cs
+++ VitalographAssessment/CaesarCipher/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿namespace Encryption.CaesarCipher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int shift = 5;
+            string output = "";
+            Console.Write("Input: ");
+            string? input = Console.ReadLine();
+            if (input != null)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] < 65 || input[i] > 90)
+                    {
+                        throw new Exception("Only A-Z supported.");
+                    }
+                    int shifted = input[i] + shift;
+                    if (shifted > 90)
+                    {
+                        shifted = 65 + shifted - 91;
+                    }
+                    output = output + (char)shifted;
+                }
+            }
+            Console.WriteLine("Output: " + output);
+        }
+    }
+}
