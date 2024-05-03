8396068af1154dfcc4e93e9a9e135a278ce417e4
SalahMahmoud22 <33803510+Salah1796@users.noreply.github.com>
2024-05-03 17:55:40 +0300

    Move Get input string from the user to sperate method

--- VitalographAssessment/CaesarCipher/Program.cs
+++ VitalographAssessment/CaesarCipher/Program.cs
@@ -7,10 +7,38 @@
             int shift = 5;
             string output = "";
             Console.Write("Input: ");
-            string? input = Console.ReadLine();
+            string input = GetInputString();
+            if (input.Equals("exit", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.WriteLine("Exiting program...");
+                return;
+            }
             if (input != null)
                 output = CaesarCipher.Encrypt(input, shift);
             Console.WriteLine("Output: " + output);
+        }
+
+        /// <summary>
+        /// Get input string from the user, ensuring it contains characters between A-Z or exit
+        /// </summary>
+        /// <returns>The input string entered by the user.</returns>
+        private static string GetInputString()
+        {
+            bool isValid = false;
+            string? input = string.Empty;
+            do
+            {
+                Console.Write("Enter a message to encrypt containing characters between A-Z (type 'exit' to close): ");
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Error: Input string cannot be null or empty.");
+                    isValid = false;
+                }
+                else
+                    isValid = true;
+            } while (!isValid);
+            return input!;
         }
     }
 }
