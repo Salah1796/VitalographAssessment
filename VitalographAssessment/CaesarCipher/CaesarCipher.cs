86724c8065bad1a18f24d01588cf1bc27089bf7c
SalahMahmoud22 <33803510+Salah1796@users.noreply.github.com>
2024-05-03 17:51:08 +0300

    Moved Encrypt method to CaesarEncryptor class

---
+++ VitalographAssessment/CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Encryption.CaesarCipher
+{
+    /// <summary>
+    /// Provides methods for encrypting messages using the Caesar cipher algorithm.
+    /// </summary>
+    public class CaesarCipher
+    {
+        /// <summary>
+        /// Encrypts the input string using the Caesar cipher algorithm with the specified shift.
+        /// </summary>
+        /// <param name="input">The input string to encrypt.</param>
+        /// <param name="shift">The shift value for encryption.</param>
+        /// <returns>The encrypted string.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the input string contains characters outside the range of A-Z.</exception>
+        public static string Encrypt(string input, int shift)
+        {
+            StringBuilder encryptedText = new();
+            foreach (char character in input)
+            {
+                if (character < 'A' || character > 'Z')
+                    throw new ArgumentOutOfRangeException($"'{character}' is out of range (Only A-Z supported.)");
+                int shifted = character + shift;
+                if (shifted > 'Z')
+                    shifted -= 26;//alphabet size
+                encryptedText.Append((char)shifted);
+            }
+            return encryptedText.ToString();
+        }
+    }
+}
--- VitalographAssessment/CaesarCipher/Program.cs
+++ VitalographAssessment/CaesarCipher/Program.cs
@@ -9,21 +9,7 @@
             Console.Write("Input: ");
             string? input = Console.ReadLine();
             if (input != null)
-            {
-                for (int i = 0; i < input.Length; i++)
-                {
-                    if (input[i] < 65 || input[i] > 90)
-                    {
-                        throw new Exception("Only A-Z supported.");
-                    }
-                    int shifted = input[i] + shift;
-                    if (shifted > 90)
-                    {
-                        shifted = 65 + shifted - 91;
-                    }
-                    output = output + (char)shifted;
-                }
-            }
+                output = CaesarCipher.Encrypt(input, shift);
             Console.WriteLine("Output: " + output);
         }
     }
